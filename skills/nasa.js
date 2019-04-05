f2b3cdcffac14b40b4578f1c14961bcccef37453
René Klose <rene.klose@gmail.com>
2019-04-05 14:14:22 +0200

    Create nasa.js

---
+++ skills/nasa.js
@@ -0,0 +1,33 @@
+var request = require('request');
+module.exports = function (controller) {
+
+    
+    controller.hears([/^nasa$/], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+            convo.say('Ok lets check the stars');
+
+                console.log(sgv);  
+
+var options = { method: 'GET',
+  url: 'https://api.nasa.gov/planetary/apod',
+  qs: { api_key: '2TmpjkT9PSZhCM7HtFzblAjmy6eW3V0H6qJvfCHi' },
+  json: true,
+  headers:  { } };
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(response.body.explanation);
+  console.log(" ");
+  console.log(response.body.hdurl)
+
+                
+                convo.say(response.body.explanation + response.body.hdurl, function (response, convo) {
+                convo.say();    
+                    convo.next();
+                    });
+                });
+    
+              })
+        });
+};
