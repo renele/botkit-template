62679e6eacaf2d36937ac7a1649673fc12d2e9fd
René Klose <rene.klose@gmail.com>
2019-04-05 12:13:58 +0200

    Update bg.js
    now working

--- skills/bg.js
+++ skills/bg.js
@@ -1,32 +1,27 @@
+var request = require('request');
 module.exports = function (controller) {
 
+    
     controller.hears([/^bg$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Ok lets check your Blood Sugar Level');
-
-var request = require('request');
-request.get({
-    url: "https://rkt1d.herokuapp.com/api/v1/entries/current.json",
-    json: true,
-    headers: {'User-Agent': 'request'}
-               }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-    var bloodsugar = data
-    }
-});
-
-
-           
-            convo.say(bloodsugar[0].sgv);
-            convo.ask('ok this looks ok!', function (response, convo) {
+            request.get({
+                url: "https://rkt1d.herokuapp.com/api/v1/entries/current.json",
+                json: true,
+                headers: {'User-Agent': 'request'}
+              }, (err, res, data) => {
+                var bloodsugar = data
+                var sgv = (bloodsugar[0].sgv);
+                console.log(sgv);  
+              
                 
-                convo.next();
-            });
+                convo.say(sgv + ' this looks ok!', function (response, convo) {
+                convo.say();    
+                    convo.next();
+                });
+    
+              })
         });
 
     });
