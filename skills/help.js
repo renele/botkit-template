3151143c93747c3c2a182c165775aebcd8b6527a
René Klose <rene.klose@gmail.com>
2019-04-07 22:07:05 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -4,7 +4,14 @@
 module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here are my skills:";
+        var text = "Here are my  Medical skills:";
+        text += "\n- " + bot.appendMention(message, "INJECT") + ": Entr amount of Insulin to DB";
+        text += "\n- " + bot.appendMention(message, "MEAL") + ": Enter amount of Carbs to DB";
+        text += "\n- " + bot.appendMention(message, "BG") + ": see current Sugar Level";
+        text += "\n- " + bot.appendMention(message, "REPORT") + ": see last to Days of BG Levels";
+        text += "\n\nI also understand some Demo Functions:";
+        text += "\n- " + bot.appendMention(message, "NASA") + ": see NASA Image of the DAY";
+        text += "\n- " + bot.appendMention(message, "NASAURL") + ": see NASA Image of the DAY unincluding link";
         text += "\n- " + bot.appendMention(message, "color") + ": ask to pick a random color";
         text += "\n- " + bot.appendMention(message, "loop") + ": example of a menu that loops until explicitly stopped";
         text += "\n- " + bot.appendMention(message, "menu") + ": implement a menu via a conversation";
@@ -13,7 +20,6 @@ module.exports = function (controller) {
         text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
         text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
         text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
-        text += "\n\nI also understand:";
         text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
