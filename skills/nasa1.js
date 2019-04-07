c3b51a234816884fd4acce2d066cb938d5600b23
René Klose <rene.klose@gmail.com>
2019-04-07 22:07:38 +0200

    Update nasa1.js

--- skills/nasa1.js
+++ skills/nasa1.js
@@ -2,7 +2,7 @@ var request = require('request');
 module.exports = function (controller) {
 
     
-    controller.hears([/^nasa$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^NASA/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Ok lets check the stars');
